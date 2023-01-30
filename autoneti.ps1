1c396f04f72bfa1ccbba8a35efd3458bbea05cc9
thatdarkw <69354549+thatdarkw@users.noreply.github.com>
2023-01-30 15:59:28 +0530

    powershell script update

--- autoneti.ps1
+++ autoneti.ps1
@@ -6,41 +6,37 @@ $keepalive="https://gateway.iitk.ac.in:1003/keepalive?080902080a3d09c7"
 $logout="https://gateway.iitk.ac.in:1003/logout?0a060c0e0b310bc5"
 
 if ($args[0] -eq "c" -or $args[0] -eq "C") {
-    $url = Invoke-WebRequest "http://google.com" -UseBasicParsing -MaximumRedirection 0 -ErrorAction SilentlyContinue
-    if ($url.StatusCode -eq 303) {
+$url = Invoke-WebRequest "http://google.com" -UseBasicParsing -MaximumRedirection 0 -ErrorAction SilentlyContinue
+    if ($url.RawContentLength -eq 140) {
         $url = Invoke-WebRequest "http://google.com" -UseBasicParsing
-        $url_resp_string = $url.ToString() -split "[`r`n]" -imatch "magic"
-        $magic_num = $url_resp_string -split '"' | select -Index 5
+        $url_resp_string = $url.ToString() -split "[`r`n]" -split '"' | select -Index 3
+        $auth_url_req = Invoke-WebRequest $url_resp_string -UseBasicParsing
+        $magic_num = $auth_url_req.ToString() -split "[`r`n]" -imatch "magic" -split '"' | select -Index 5
         $con_req = Invoke-WebRequest -Method Post -Body "magic=$magic_num&username=$username&password=$password" https://gateway.iitk.ac.in:1003/ -UseBasicParsing
         $logout = $con_req.Links | Select-Object -ExpandProperty href | select -Index 1
 
+
         $keepalive_find = $con_req.ToString() -split "[`r`n]" -imatch "location.href="
-        $keepalive = $keepalive_find -split '"' | select -Index 1
         echo "Connected..."
         echo $keepalive
     }
+
     else {
        echo "Already connected..."
        echo $keepalive
     }
 
     while($true){
-    curl $keepalive -UseBasicParsing | Out-Null
-    Start-Sleep -Seconds 500
+        curl $keepalive -UseBasicParsing | Out-Null
+        Start-Sleep -Seconds 500
     }
-
 }
 
 elseif ($args[0] -eq "d" -or $args[0] -eq "D") {
     $logout_req = curl $logout -UseBasicParsing -MaximumRedirection 0 -ErrorAction SilentlyContinue
-    if ($logout_req.StatusCode -eq 200) {
-        echo "Disconnected..."
-    }
-    else {
-        echo "Probably not connected... Connect first..."
-    }
+    echo "Disconnected..."
 }
+
 else {
     echo "Pass either c to connect or d to disconnect."
 }
-
